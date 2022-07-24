333d2ea99078b884c4b10fb9e40e1a4ef08a0968
jampee0915 <jun19900915@gmail.com>
2022-07-24 21:04:56 +0900

    Add flyway settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,10 @@ rootProject.name = "ddd-kotlin-sample"
 
 include(
     "ddd-domain",
-    "ddd-infra",
-    "ddd-usecase",
-    "ddd-presentation",
     "ddd-jooq-codes",
     "ddd-jooq-gen",
-    "ddd-migration"
+    "ddd-migration",
+    "ddd-infra",
+    "ddd-usecase",
+    "ddd-presentation"
 )
